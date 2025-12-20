3abc5f064c32618d0012a5b005a46272eaee2d05
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-12-20 18:26:36 -0500

    Add database guitar to component Guitar.jsx

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,21 +1,20 @@
-export default function Guitar() {
+export default function Guitar({ guitar }) {
+  const { name, image, description, price } = guitar;
+
   return (
     <>
       <div className="align-items-center my-4 col-md-6 col-lg-4 row">
         <div className="col-4">
           <img
             className="img-fluid"
-            src="/img/guitarra_01.jpg"
+            src={`/img/${image}.jpg`}
             alt="imagen guitarra"
           />
         </div>
         <div className="col-8">
-          <h3 className="text-black text-uppercase fs-4 fw-bold">Lukather</h3>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae
-            labore odit magnam in autem nesciunt, amet deserunt
-          </p>
-          <p className="text-primary fw-black fs-3">$299</p>
+          <h3 className="text-black text-uppercase fs-4 fw-bold">{name}</h3>
+          <p>{description}</p>
+          <p className="text-primary fw-black fs-3">${price}</p>
           <button type="button" className="w-100 btn btn-dark">
             Agregar al Carrito
           </button>
